c9e88ef4be2e1b9b09ba1dc2a952110d287d65cd
bunnie <bunnie@kosagi.com>
2025-05-30 02:05:22 +0800

    [otbn] modify dmem_dump routines to match what's used in the PQ working repo
    Here, I think there may be some divergence between what's in
    expo and what's in the PQ working repo. The actual format of
    the dmem expected data takes the form of address ranges followed
    by hex numbers. What the original code seemed to expect is actually
    a single large hex block.
    
    The code from the working repo is pulled into expo in this commit,
    but this may not actually be the correct decision.
    
    Furthermore, there is a strange problem where parse_actual_dump()
    is handed an ASCII object, but the type is 'bytes'. There is
    a mismatch in the abstractions: what we get is ASCII data that looks
    like this:
    
    dmem:
    04112345...
    
    So there is literally the six bytes b'dmem:\n' are in the stream,
    and the data being passed in is ASII characters representing
    the binary data.
    
    A converter which strips off the first 6 bytes, decodes the ASCII
    and turns it into the binary data is inserted into parse_actual_dmem().
    
    This feels hacky and wrong; but I'll commit this here and leave it
    up for reviewers to comment on the correct method to fix this.
    
    Signed-off-by: bunnie <bunnie@kosagi.com>

--- hw/ip/otbn/util/shared/dmem_dump.py
+++ hw/ip/otbn/util/shared/dmem_dump.py
@@ -4,42 +4,53 @@
 
 import re
 import struct
-from typing import Dict
+from typing import List
 
 from hw.ip.otbn.util.shared.mem_layout import get_memory_layout
 
-_DMEM_RE = re.compile(
-    r'\s*(?P<label>[a-zA-Z0-9_]+)\s*:\s*(?P<val>(:?[0-9a-f]+))$')
+_DMEM_RE = re.compile(r'\s*(?P<start>\d+)-(?P<end>\d+)\s*=\s*(?P<value>(:?0x[0-9a-f]+))$')
 
+def parse_dmem_exp(exp: str) -> List[int]:
+    '''Parse expected output definition for dmem.
 
-def parse_dmem_exp(dump: str) -> Dict[str, int]:
-    '''Parse the expected dmem.
+    Expects each line to be of the form <index_start>-<index_end> = <value>,
+    where <index_{start,end}> are decimal byte offsets defining the dmem range
+    and <value> defining the data to be present in this memory region given as
+    hexadecimal with leading '0x' and padded to the size of the range.
 
-    Format:
-        label: hex_data
+    Ranges may appear in arbitrary order but must not overlap. Comments use
+    '#'; any content in a line following '#' will be ignored.
 
-    Returns a dictionary mapping labels to the expected bytes.
+    Returns a list of size `get_memory_layout().dmem_size_bytes` where each
+    element represents one byte of the expected dmem. Undefined bytes are
+    initialized as None.
     '''
+    dmem_bytes = [None] * get_memory_layout().dmem_size_bytes
 
-    out = {}
-    for line in dump.split('\n'):
+    for line in exp.split('\n'):
         # Remove comments and ignore blank lines.
         line = line.split('#', 1)[0].strip()
         if not line:
             continue
+
         m = _DMEM_RE.match(line)
         if not m:
-            raise ValueError(f'Failed to parse dmem dump line ({line}).')
-        label = m.group('label')
-        value = bytes.fromhex(m.group('val'))
-        value = value[::-1]  # big-endian -> little-endian
+            raise ValueError(f'Failed to parse dmem dump line ({line:!r}).')
 
-        if label in out:
-            raise ValueError(f'DMEM dump contains multiple values '
-                             f'for {label}.')
-        out[label] = value
+        start = int(m.group("start"))
+        end = int(m.group("end"))
+        value = m.group("value")[2:]
 
-    return out
+        if len(value) // 2 != ((end + 1) - start):
+            raise ValueError(f'Range does not match length of value:\
+                             {len(value) // 2} vs. {(end + 1) - start}')
+
+        if any(dmem_bytes[start:(end + 1)]):
+            raise ValueError('Ranges overlapping.')
+
+        dmem_bytes[start:(end + 1)] = [int(value[i:i + 2], 16) for i in
+                                       range(0, len(value), 2)]
+    return dmem_bytes
 
 
 def parse_actual_dmem(dump: bytes) -> bytes:
@@ -48,9 +59,12 @@ def parse_actual_dmem(dump: bytes) -> bytes:
     Returns the dmem bytes except integrity info.
     '''
     dmem_bytes = []
+    hexdump = dump[6:]
+    bindump =  bytes.fromhex(hexdump.decode('ascii'))
     # 8 32-bit data words + 1 byte integrity info per word = 40 bytes
     bytes_w_integrity = 8 * 4 + 8
-    for w in struct.iter_unpack(f"<{bytes_w_integrity}s", dump):
+    for w in struct.iter_unpack(f"<{bytes_w_integrity}s", bindump):
+        tmp = []
         # discard byte indicating integrity status
         for v in struct.iter_unpack("<BI", w[0]):
             dmem_bytes += v[1].to_bytes(4, "little")
