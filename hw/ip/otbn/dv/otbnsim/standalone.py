3ed3382e0ec125d86566225ed4c04245639f9012
bunnie <bunnie@kosagi.com>
2025-05-31 01:38:28 +0800

    [otbn] align the output of otbnsim dmem range with what upstream expects
    somewhere along the way this output file got munged into an
    ascii text format which isn't actually the supported format
    used by the bazel tools to compare outputs.
    
    Signed-off-by: bunnie <bunnie@kosagi.com>

--- hw/ip/otbn/dv/otbnsim/standalone.py
+++ hw/ip/otbn/dv/otbnsim/standalone.py
@@ -30,7 +30,7 @@ def main() -> int:
     parser.add_argument(
         '--dump-dmem',
         metavar="FILE",
-        type=argparse.FileType('w'),
+        type=argparse.FileType('wb'),
         help=("after execution, write the data memory contents to this file. "
               "Use '-' to write to STDOUT.")
     )
@@ -90,8 +90,7 @@ def main() -> int:
             return 1
 
     if args.dump_dmem is not None:
-        args.dump_dmem.write("dmem:\n")
-        args.dump_dmem.write(sim.dump_data().hex())
+        args.dump_dmem.write(sim.dump_data())
 
     if collect_stats:
         assert sim.stats is not None
