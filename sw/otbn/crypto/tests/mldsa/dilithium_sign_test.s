8969af63e7e1bb524756c2b8a55f88786f0a25ad
Jade Philipoom <jadep@zerorisc.com>
2025-06-05 16:57:48 +0200

    [otbn] Fix copyright headers on a couple of files.
    While checking the headers, I noticed that these failed because of an outdated
    lowRISC copyright format. However, on closer inspection, the files seem to be
    entirely new content (which is why the earlier rebase didn't add the new
    format) and have a "Copyright" line instead of a "Modified" line on them. My
    best understanding is that this is a mistake and the "authors" copyright was
    supposed to go on top like it does for the other new files, so I'm correcting
    it here according to that understanding.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>

--- sw/otbn/crypto/tests/mldsa/dilithium_sign_test.s
+++ sw/otbn/crypto/tests/mldsa/dilithium_sign_test.s
@@ -1,8 +1,6 @@
-/* Copyright lowRISC contributors. */
+/* Copyright "Towards ML-KEM & ML-DSA on OpenTitan" Authors */
 /* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
 /* SPDX-License-Identifier: Apache-2.0 */
-/* Modified by Authors of "Towards ML-KEM & ML-DSA on OpenTitan" (https://eprint.iacr.org/2024/1192) */
-/* Copyright "Towards ML-KEM & ML-DSA on OpenTitan" Authors */
 
 
 /**
