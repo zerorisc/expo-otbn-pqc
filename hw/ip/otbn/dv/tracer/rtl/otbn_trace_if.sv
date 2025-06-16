2590c23debc6593d268265ad34984b1bd0241ae5
Ruben <ruben@polycephaly.org>
2025-06-16 15:51:30 +0800

    first hw-decode draft

--- hw/ip/otbn/dv/tracer/rtl/otbn_trace_if.sv
+++ hw/ip/otbn/dv/tracer/rtl/otbn_trace_if.sv
@@ -275,8 +275,8 @@ interface otbn_trace_if
   // For ISPR reads look at the ACC flops directly. For other ACC reads look at the `acc_blanked`
   // signal in order to read ACC as 0 for the BN.MULQACC.Z instruction variant.
   assign ispr_read_data[IsprAcc] =
-      (any_ispr_read & (ispr_addr == IsprAcc)) ? u_otbn_mac_bignum.acc_no_intg_q  :
-                                                 u_otbn_mac_bignum.acc_blanked;
+      (any_ispr_read & (ispr_addr == IsprAcc)) ? u_otbn_mac_bignum.acc_no_intg_q[255:0]  :  // FIX ME! ACC0 and ACC1!
+                                                 u_otbn_mac_bignum.acc_blanked[255:0];      // FIX ME! ACC0 and ACC1!
 
   assign ispr_write[IsprRnd] = 1'b0;
   assign ispr_write_data[IsprRnd] = '0;
