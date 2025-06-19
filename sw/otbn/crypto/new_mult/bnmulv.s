bbfd1c9a55e2d992b58932d881bcc5673aa68d0a
phamhnh <nguyenhien.phamhoang@gmail.com>
2025-06-19 22:56:57 +0200

    [otbn] Update bnmulv and bnmulv_test after removing ACCL

--- sw/otbn/crypto/new_mult/bnmulv.s
+++ sw/otbn/crypto/new_mult/bnmulv.s
@@ -33,121 +33,121 @@ test_bnmulv_8S:
   /* 1 -- 0: acc -- standard */
   bn.mulv.8S.even.acc w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.8S.odd.acc w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 1: acc -- .lo */
   bn.mulv.8S.even.acc.lo w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.8S.odd.acc.lo w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 2: acc -- .hi */
   bn.mulv.8S.even.acc.hi w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.8S.odd.acc.hi w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 3: acc -- .hi.cond */
   bn.mulv.8S.even.acc.hi.cond w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.8S.odd.acc.hi.cond w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 0: acc.z -- standard */
   bn.mulv.8S.even.acc.z w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.8S.odd.acc.z w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 1: acc.z -- .lo */
   bn.mulv.8S.even.acc.z.lo w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.8S.odd.acc.z.lo w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 2: acc.z -- .hi */
   bn.mulv.8S.even.acc.z.hi w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.8S.odd.acc.z.hi w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 3: acc.z -- .hi.cond */
   bn.mulv.8S.even.acc.z.hi.cond w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.8S.odd.acc.z.hi.cond w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   ret
@@ -176,79 +176,79 @@ test_bnmulv_16H:
   /* 1 -- 0: acc -- standard */
   bn.mulv.16H.even.acc w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.16H.odd.acc w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 1: acc -- .lo */
   bn.mulv.16H.acc.lo w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 2: acc -- .hi */
   bn.mulv.16H.acc.hi w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 3: acc -- .hi.cond */
   bn.mulv.16H.acc.hi.cond w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 0: acc.z -- standard */
   bn.mulv.16H.even.acc.z w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.16H.odd.acc.z w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 1: acc.z -- .lo */
   bn.mulv.16H.acc.z.lo w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 2: acc.z -- .hi */
   bn.mulv.16H.acc.z.hi w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 3: acc.z -- .hi.cond */
   bn.mulv.16H.acc.z.hi.cond w2, w0, w1
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   ret
@@ -286,121 +286,121 @@ test_bnmulvl_8S:
   /* 1 -- 0: acc -- standard */
   bn.mulv.l.8S.even.acc w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.8S.odd.acc w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 1: acc -- .lo */
   bn.mulv.l.8S.even.acc.lo w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.8S.odd.acc.lo w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 2: acc -- .hi */
   bn.mulv.l.8S.even.acc.hi w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.8S.odd.acc.hi w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 3: acc -- .hi.cond */
   bn.mulv.l.8S.even.acc.hi.cond w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.8S.odd.acc.hi.cond w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 0: acc.z -- standard */
   bn.mulv.l.8S.even.acc.z w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.8S.odd.acc.z w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 1: acc.z -- .lo */
   bn.mulv.l.8S.even.acc.z.lo w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.8S.odd.acc.z.lo w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 2: acc.z -- .hi */
   bn.mulv.l.8S.even.acc.z.hi w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.8S.odd.acc.z.hi w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 3: acc.z -- .hi.cond */
   bn.mulv.l.8S.even.acc.z.hi.cond w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.8S.odd.acc.z.hi.cond w2, w0, sw0.5
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   ret
@@ -429,79 +429,79 @@ test_bnmulvl_16H:
   /* 1 -- 0: acc -- standard */
   bn.mulv.l.16H.even.acc w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.16H.odd.acc w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 1: acc -- .lo */
   bn.mulv.l.16H.acc.lo w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 2: acc -- .hi */
   bn.mulv.l.16H.acc.hi w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 1 -- 3: acc -- .hi.cond */
   bn.mulv.l.16H.acc.hi.cond w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 0: acc.z -- standard */
   bn.mulv.l.16H.even.acc.z w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   bn.mulv.l.16H.odd.acc.z w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 1: acc.z -- .lo */
   bn.mulv.l.16H.acc.z.lo w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 2: acc.z -- .hi */
   bn.mulv.l.16H.acc.z.hi w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   /* 2 -- 3: acc.z -- .hi.cond */
   bn.mulv.l.16H.acc.z.hi.cond w2, w0, sw1.3
   bn.sid x6, 0(x7++)
-  bn.wsrr w3, 0xb
+  bn.wsrr w3, 0x3
   bn.sid x8, 0(x7++)
-  bn.wsrr w3, 0xc
+  bn.wsrr w3, 0xb
   bn.sid x8, 0(x7++)
 
   ret
