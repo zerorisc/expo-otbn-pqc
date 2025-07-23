10c16bea0885d35370692b86698918aff74d08c2
Evan Apinis <eapinis@zerorisc.com>
2025-07-23 18:53:06 +0000

    [otbn] Changed base address for darjeerling with increased memory size.
    Increased the memory allocation size for OTBN in top_darjeerling.gen.hjson from 65K
    to 256K due to the increased IMEM and DMEM sizes. Moved the OTBN base address to
    prevent overlap.
    
    Signed-off-by: Evan Apinis <eapinis@zerorisc.com>

--- hw/top_darjeeling/sw/autogen/chip/top_darjeeling.rs
+++ hw/top_darjeeling/sw/autogen/chip/top_darjeeling.rs
@@ -389,7 +389,7 @@ pub const KMAC_SIZE_BYTES: usize = 0x1000;
 ///
 /// This should be used with #mmio_region_from_addr to access the memory-mapped
 /// registers associated with the peripheral (usually via a DIF).
-pub const OTBN_BASE_ADDR: usize = 0x21130000;
+pub const OTBN_BASE_ADDR: usize = 0x22100000;
 
 /// Peripheral size for otbn in top darjeeling.
 ///
@@ -397,7 +397,7 @@ pub const OTBN_BASE_ADDR: usize = 0x21130000;
 /// memory-mapped registers associated with this peripheral should have an
 /// address between #OTBN_BASE_ADDR and
 /// `OTBN_BASE_ADDR + OTBN_SIZE_BYTES`.
-pub const OTBN_SIZE_BYTES: usize = 0x10000;
+pub const OTBN_SIZE_BYTES: usize = 0x40000;
 
 /// Peripheral base address for keymgr_dpe in top darjeeling.
 ///
