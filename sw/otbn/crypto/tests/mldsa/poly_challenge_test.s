da08f368bc356c9c56644108f41b897133ed07cc
Jade Philipoom <jadep@zerorisc.com>
2025-07-25 19:21:28 +0200

    [mldsa] Add a compact version of the challenge computation and a test.
    The alternate implementation is not yet hooked up to the signing algorithm.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>

---
+++ sw/otbn/crypto/tests/mldsa/poly_challenge_test.s
@@ -0,0 +1,344 @@
+/**
+ * Standalone test for sampling the ML-DSA challenge polynomial.
+ */
+
+#define SEEDBYTES 32
+#define CRHBYTES 64
+#define TRBYTES 64
+#define RNDBYTES 32
+#define N 256
+#define Q 8380417
+#define D 13
+#define ROOT_OF_UNITY 1753
+
+#if DILITHIUM_MODE == 2
+#define K 4
+#define L 4
+#define ETA 2
+#define TAU 39
+#define BETA 78
+#define GAMMA1 131072
+#define GAMMA2 95232
+#define OMEGA 80
+#define CTILDEBYTES 32
+
+#elif DILITHIUM_MODE == 3
+#define K 6
+#define L 5
+#define ETA 4
+#define TAU 49
+#define BETA 196
+#define GAMMA1 524288
+#define GAMMA2 261888
+#define OMEGA 55
+#define CTILDEBYTES 48
+
+#elif DILITHIUM_MODE == 5
+#define K 8
+#define L 7
+#define ETA 2
+#define TAU 60
+#define BETA 120
+#define GAMMA1 524288
+#define GAMMA2 261888
+#define OMEGA 75
+#define CTILDEBYTES 64
+
+#endif
+
+#define POLYT1_PACKEDBYTES  320
+#define POLYT0_PACKEDBYTES  416
+#define POLYVECH_PACKEDBYTES (OMEGA + K)
+
+#if GAMMA1 == (1 << 17)
+#define POLYZ_PACKEDBYTES   576
+#elif GAMMA1 == (1 << 19)
+#define POLYZ_PACKEDBYTES   640
+#endif
+
+#if GAMMA2 == (Q-1)/88
+#define POLYW1_PACKEDBYTES  192
+#elif GAMMA2 == (Q-1)/32
+#define POLYW1_PACKEDBYTES  128
+#endif
+
+#if ETA == 2
+#define POLYETA_PACKEDBYTES  96
+#elif ETA == 4
+#define POLYETA_PACKEDBYTES 128
+#endif
+
+.section .text.start
+
+main:
+  /* Init all-zero register. */
+  bn.xor    w31, w31, w31
+
+  /* Compute the challenge polynomial. */
+  la x10, ctilde
+  jal x1, poly_challenge_compact
+
+  ecall
+
+.data
+
+.balign 32
+ctilde:
+  .word 0x591e9725
+  .word 0x10920a48
+  .word 0xd9d0d233
+  .word 0x5979d60d
+  .word 0x25c355b5
+  .word 0x8f331a02
+  .word 0xc68efd33
+  .word 0x75852224
+
+/* Below this line, just other constants that the poly library or its dependencies expect. */
+
+.balign 32
+.global modulus
+modulus:
+  .word 0x007fe001
+  .word 0x007fe001
+  .word 0x007fe001
+  .word 0x007fe001
+  .word 0x007fe001
+  .word 0x007fe001
+  .word 0x007fe001
+  .word 0x007fe001
+
+.global reduce32_const
+reduce32_const:
+    .word 0x1
+    .word 0x1
+    .word 0x1
+    .word 0x1
+    .word 0x1
+    .word 0x1
+    .word 0x1
+    .word 0x1
+
+.global power2round_D
+power2round_D:
+    .word 0xd
+    .word 0xd
+    .word 0xd
+    .word 0xd
+    .word 0xd
+    .word 0xd
+    .word 0xd
+    .word 0xd
+.global power2round_D_preprocessed
+power2round_D_preprocessed:
+    .word 0xfff
+    .word 0xfff
+    .word 0xfff
+    .word 0xfff
+    .word 0xfff
+    .word 0xfff
+    .word 0xfff
+    .word 0xfff
+.global eta
+eta:
+    .word ETA
+    .word ETA
+    .word ETA
+    .word ETA
+    .word ETA
+    .word ETA
+    .word ETA
+    .word ETA
+.global polyt0_pack_const
+polyt0_pack_const:
+    .word 0x1000
+    .word 0x1000
+    .word 0x1000
+    .word 0x1000
+    .word 0x1000
+    .word 0x1000
+    .word 0x1000
+    .word 0x1000
+.global decompose_const
+decompose_const:
+#if GAMMA2 == (Q-1)/88
+    .word 0x00002c0b
+    .word 0x00002c0b
+    .word 0x00002c0b
+    .word 0x00002c0b
+    .word 0x00002c0b
+    .word 0x00002c0b
+    .word 0x00002c0b
+    .word 0x00002c0b
+#elif GAMMA2 == (Q-1)/32
+    .word 1025
+    .word 1025
+    .word 1025
+    .word 1025
+    .word 1025
+    .word 1025
+    .word 1025
+    .word 1025
+#endif
+.global gamma1_vec_const
+gamma1_vec_const:
+    .word GAMMA1
+    .word GAMMA1
+    .word GAMMA1
+    .word GAMMA1
+    .word GAMMA1
+    .word GAMMA1
+    .word GAMMA1
+    .word GAMMA1
+.global gamma2_vec_const
+gamma2_vec_const:
+    .word GAMMA2
+    .word GAMMA2
+    .word GAMMA2
+    .word GAMMA2
+    .word GAMMA2
+    .word GAMMA2
+    .word GAMMA2
+    .word GAMMA2
+.global qm1half_const
+qm1half_const:
+    .word 0x003ff000
+    .word 0x003ff000
+    .word 0x003ff000
+    .word 0x003ff000
+    .word 0x003ff000
+    .word 0x003ff000
+    .word 0x003ff000
+    .word 0x003ff000
+.global decompose_127_const
+decompose_127_const:
+    .word 0x0000007f
+    .word 0x0000007f
+    .word 0x0000007f
+    .word 0x0000007f
+    .word 0x0000007f
+    .word 0x0000007f
+    .word 0x0000007f
+    .word 0x0000007f
+.global decompose_43_const
+decompose_43_const:
+#if GAMMA2 == (Q-1)/88
+    .word 0x0000002b
+    .word 0x0000002b
+    .word 0x0000002b
+    .word 0x0000002b
+    .word 0x0000002b
+    .word 0x0000002b
+    .word 0x0000002b
+    .word 0x0000002b
+#elif GAMMA2 == (Q-1)/32
+    .word 0x000000f
+    .word 0x000000f
+    .word 0x000000f
+    .word 0x000000f
+    .word 0x000000f
+    .word 0x000000f
+    .word 0x000000f
+    .word 0x000000f
+#endif
+.global polyeta_unpack_mask
+polyeta_unpack_mask:
+#if ETA == 2
+    .word 0x07
+    .word 0x07
+    .word 0x07
+    .word 0x07
+    .word 0x07
+    .word 0x07
+    .word 0x07
+    .word 0x07
+#elif ETA == 4
+    .word 0x0f
+    .word 0x0f
+    .word 0x0f
+    .word 0x0f
+    .word 0x0f
+    .word 0x0f
+    .word 0x0f
+    .word 0x0f
+#endif
+.global polyt1_unpack_dilithium_mask
+polyt1_unpack_dilithium_mask:
+    .word 0x3ff
+    .word 0x3ff
+    .word 0x3ff
+    .word 0x3ff
+    .word 0x3ff
+    .word 0x3ff
+    .word 0x3ff
+    .word 0x3ff
+.global polyt0_unpack_dilithium_mask
+polyt0_unpack_dilithium_mask:
+    .word 0x1fff
+    .word 0x1fff
+    .word 0x1fff
+    .word 0x1fff
+    .word 0x1fff
+    .word 0x1fff
+    .word 0x1fff
+    .word 0x1fff
+.global polyz_unpack_dilithium_mask
+polyz_unpack_dilithium_mask:
+#if GAMMA1 == (1 << 17)
+    .word 0x3ffff
+    .word 0x3ffff
+    .word 0x3ffff
+    .word 0x3ffff
+    .word 0x3ffff
+    .word 0x3ffff
+    .word 0x3ffff
+    .word 0x3ffff
+#elif GAMMA1 == (1 << 19)
+    .word 0xfffff
+    .word 0xfffff
+    .word 0xfffff
+    .word 0xfffff
+    .word 0xfffff
+    .word 0xfffff
+    .word 0xfffff
+    .word 0xfffff
+#endif
+.global poly_uniform_eta_205
+poly_uniform_eta_205:
+    .word 205
+    .word 205
+    .word 205
+    .word 205
+    .word 205
+    .word 205
+    .word 205
+    .word 205
+.global poly_uniform_eta_2
+poly_uniform_eta_2:
+    .word 2
+    .word 2
+    .word 2
+    .word 2
+    .word 2
+    .word 2
+    .word 2
+    .word 2
+.global poly_uniform_eta_5
+poly_uniform_eta_5:
+    .word 5
+    .word 5
+    .word 5
+    .word 5
+    .word 5
+    .word 5
+    .word 5
+    .word 5
+.global poly_wdr2gpr
+poly_wdr2gpr:
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
+    .word 0
