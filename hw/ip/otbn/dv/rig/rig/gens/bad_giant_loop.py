c11a5d9e282b13695a60630ecc124fd1a68470a0
Evan Apinis <eapinis@zerorisc.com>
2025-11-04 20:13:15 +0000

    [otbn] Added copyright headers to files
    Signed-off-by: Evan Apinis <eapinis@zerorisc.com>

--- hw/ip/otbn/dv/rig/rig/gens/bad_giant_loop.py
+++ hw/ip/otbn/dv/rig/rig/gens/bad_giant_loop.py
@@ -1,4 +1,5 @@
 # Copyright lowRISC contributors (OpenTitan project).
+# Copyright zeroRISC Inc.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
