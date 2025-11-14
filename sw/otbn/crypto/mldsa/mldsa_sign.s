2deb6e3f6a08978a6f369ceef185774da0f7aee4
Jade Philipoom <jadep@zerorisc.com>
2025-11-14 11:25:18 +0100

    [mldsa] Adjust polynomial library so that stack alignment is not necessary.
    This is a reapplication of the transformations in commit 51cd78 to the code
    with the new multiplier.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>

--- sw/otbn/crypto/mldsa/mldsa_sign.s
+++ sw/otbn/crypto/mldsa/mldsa_sign.s
@@ -591,8 +591,9 @@ _rej_crypto_sign_signature_internal:
     addi a2, s11, 0 /* Compute nonce */
     la   a3, gamma1_vec_const
 
-    LOOPI L, 2
+    LOOPI L, 3
         jal  x1, poly_uniform_gamma_1
+        addi a0, a0, 1024
         addi a2, a2, 1 /* a2 should be preserved after execution */
 
     addi s11, s11, L
