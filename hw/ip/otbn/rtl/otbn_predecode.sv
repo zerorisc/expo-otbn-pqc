52597df5273a7fb4ffe20e9d21ebbfc1b9e4069a
Ruben <ruben@polycephaly.org>
2025-11-18 11:17:58 +0800

    clean up

--- hw/ip/otbn/rtl/otbn_predecode.sv
+++ hw/ip/otbn/rtl/otbn_predecode.sv
@@ -464,13 +464,13 @@ module otbn_predecode
             end
             3'b010: begin  // BN.LD and BN.SD
               rf_ren_a_base        = 1'b1;
+
               if (imem_rdata_i[7]) begin // BN.SD
                 rf_ren_b_bignum        = 1'b1;
-              //end else begin      // BN.LD
               end
+
               lsu_addr_en_predec_o = 1'b1;
 
-              //if (imem_rdata_i[21]) begin
               if (imem_rdata_i[8]) begin
                 rf_we_a_base = 1'b1;
               end
