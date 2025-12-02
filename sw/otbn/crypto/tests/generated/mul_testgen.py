3e996e57e9b58c74bc5be54b2fd0a3bf9c77bcd3
Jade Philipoom <jadep@zerorisc.com>
2025-12-02 13:56:21 +0100

    [otbn] Add expected-memory support to testgen infrastructure.
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>

--- sw/otbn/crypto/tests/generated/mul_testgen.py
+++ sw/otbn/crypto/tests/generated/mul_testgen.py
@@ -7,13 +7,13 @@ import argparse
 import random
 from typing import TextIO, Optional
 
-from shared.testgen import write_test_data, write_test_exp
+from shared.testgen import write_test_data, write_test_exp, write_test_dexp
 
 OPERAND_LIMBS = 2
 LIMB_NBYTES = 32
 
 
-def gen_mul_test(seed: Optional[int], data_file: TextIO, exp_file: TextIO):
+def gen_mul_test(seed: Optional[int], data_file: TextIO, exp_file: TextIO, dexp_file: TextIO):
     # Generate random operands.
     if seed is not None:
         random.seed(seed)
@@ -27,12 +27,15 @@ def gen_mul_test(seed: Optional[int], data_file: TextIO, exp_file: TextIO):
     inputs = {'x': x_bytes, 'y': y_bytes}
     write_test_data(inputs, data_file)
 
-    # Write expected output values.
+    # Write expected register values.
     xy_bytes = int.to_bytes(x * y, byteorder='little', length=operand_nbytes * 2)
     exp = {}
     for i in range(OPERAND_LIMBS * 2):
         exp[f'w{i}'] = xy_bytes[i * LIMB_NBYTES:(i + 1) * LIMB_NBYTES]
     write_test_exp(exp, exp_file)
+
+    # Write expected dmem values (none).
+    write_test_dexp({}, dexp_file)
 
 
 if __name__ == '__main__':
@@ -49,6 +52,10 @@ if __name__ == '__main__':
                         metavar='FILE',
                         type=argparse.FileType('w'),
                         help=('Output file for expected register values.'))
+    parser.add_argument('dexp',
+                        metavar='FILE',
+                        type=argparse.FileType('w'),
+                        help=('Output file for expected DMEM values.'))
     args = parser.parse_args()
 
-    gen_mul_test(args.seed, args.data, args.exp)
+    gen_mul_test(args.seed, args.data, args.exp, args.dexp)
