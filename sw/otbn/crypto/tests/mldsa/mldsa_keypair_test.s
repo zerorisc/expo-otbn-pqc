b648315d591a81c9fc030666afe1a4423c07602d
Jade Philipoom <jadep@zerorisc.com>
2025-12-02 11:53:17 +0100

    [mldsa] Make keypair and verify use external data.
    This is a helpful stepping stone for enabling random testing.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>

--- sw/otbn/crypto/tests/mldsa/mldsa_keypair_test.s
+++ sw/otbn/crypto/tests/mldsa/mldsa_keypair_test.s
@@ -130,15 +130,3 @@ pk:
 .globl sk
 sk:
   .zero CRYPTO_SECRETKEYBYTES
-
-.balign 32
-.globl zeta
-zeta:
-  .word 0xa42b9c7f
-  .word 0x7d828fe8
-  .word 0x50456061
-  .word 0x3e850576
-  .word 0x93803bd7
-  .word 0x88bceff6
-  .word 0xac6e1aeb
-  .word 0x26ef66fa
