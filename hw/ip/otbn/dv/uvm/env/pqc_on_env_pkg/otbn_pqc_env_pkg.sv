09e63599a26de4dc0ee7e41609f5f1c603f08abf
Evan Apinis <eapinis@zerorisc.com>
2025-12-09 21:47:33 +0000

    [otbn] Replaced +define usage with new package and fusesoc virtual cores.
    Signed-off-by: Evan Apinis <eapinis@zerorisc.com>

---
+++ hw/ip/otbn/dv/uvm/env/pqc_on_env_pkg/otbn_pqc_env_pkg.sv
@@ -0,0 +1,9 @@
+// Copyright zeroRISC Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+package otbn_pqc_env_pkg;
+  // Enable PQC mode for the TB and DUT
+  parameter bit OtbnPQCEn = 1;
+
+endpackage
